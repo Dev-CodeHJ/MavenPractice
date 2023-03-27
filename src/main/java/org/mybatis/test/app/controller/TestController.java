7aebddcae8feae953b543959802f4ff812a49379
Dev-CodeHJ <rlagnlwls0127@naver.com>
2023-03-27 18:03:01 +0900

    jsp ing..

--- src/main/java/org/mybatis/test/app/controller/TestController.java
+++ src/main/java/org/mybatis/test/app/controller/TestController.java
@@ -1,13 +1,14 @@
 package org.mybatis.test.app.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Test {
+public class TestController {
 
-//    @GetMapping("/index")
-//    public String test() {
-//        return "Hello";
-//    }
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
